24f1783d59ae7dfdb685017a8f66c7df9ff78b3e
flyingDutchman01 <nirjalprajapati@gmail.com>
2022-02-26 13:18:03 +0545

    removed callback from on sendmessage

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
 
     socket.on('join',({name,room},callback)=>{
+        console.log(name,room);
         const {error, user} = addUser({id:socket.id, name, room});
 
         if(error) return callback(error);
@@ -43,12 +44,11 @@ io.on('connection', (socket) => {
         
     });
 
-    socket.on('sendMessage', (message,callback) => {
+    socket.on('sendMessage', (message) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', {user: user.name, text:message});
 
-        callback();
     })
 
     socket.on('disconnect',()=>{
